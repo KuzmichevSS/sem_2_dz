f5fbf0359918f787b11f1c3a2f6488240a51cca1
Sergey Kuzmichev <kyz1979@mail.ru>
2023-01-31 23:52:07 +0300

    задача 3

---
+++ Z-3/Program.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.Write("Введите номер дня недели: ");
+int Day = Convert.ToInt32(Console.ReadLine());
+if (Day >= 1 && Day <=7) {
+	if (Day >= 6) {
+		Console.Write("Выходной день");
+	} else {
+		Console.Write("Рабочий день");
+	}
+} else {
+	Console.Write("Вводите правильно! от 1 до 7.");
+}
